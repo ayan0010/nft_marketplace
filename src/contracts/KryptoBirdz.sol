1064cad7c019cd29d9b27c195daffe881bc431a9
itzam10 <resoayan@gmail.com>
2022-08-09 15:49:29 +0530

    test

--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -1,14 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Kryptobird {
-    // this contract gonna hold all our ERC721 functionality,interfaces,metadata,etc.
-    string public name;
-    string public symbol;
+import './ERC721Connector.sol';
+contract Kryptobird is ERC721Connector{
+ 
+   constructor()  ERC721Connector ('KryptoBird','KBIRDZ'){
 
-    constructor(){
-        name='KryptoBird';
-        symbol='KBIRDZ';
-    }
+   }
 
 }
